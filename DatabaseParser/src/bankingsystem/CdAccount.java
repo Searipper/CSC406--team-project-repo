f71d15bc196a4afd8aeece8092c3ef964c6e6276
Martin <mlau@missouriwestern.edu>
2012-12-04 13:12:42 -0600

    Updated CdAccounts file
    Sets rollover and account creation time, properly formatted.

--- DatabaseParser/src/bankingsystem/CdAccount.java
+++ DatabaseParser/src/bankingsystem/CdAccount.java
@@ -1,7 +1,9 @@
 package bankingsystem;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -13,38 +15,109 @@ import java.util.Date;
  */
 public class CdAccount extends AbstractAccount {
 
+    //---------------------------------
+    //  Variable decleration
+    //---------------------------------
     final double PENALTY = 90; //temp constant for penalty to apply to early CD withdrawal
-    final int FIXEDMAT = (365 * 5); //standard maturity date if none set
-    private long endDate; // date that CD reaches full maturity
+    final long FIXEDMAT = 5; //standard maturity date if none set
+    private long longEndDate; // date that CD reaches full maturity
+    Date endDate = new Date();
     private double interestEarned; //variable to store interest earned
     private double interestRate = .015; //changable to whatever
+    long longRolloverDate; // date the the CD will rollover once it has reached maturity, should be ten days standard
+    Date rolloverDate = new Date();
 
+    //---------------------------------
+    //  Constructor
+    //---------------------------------
     public CdAccount(int customerID, int accountNum, double balance, int accountFlag, double interest, long maturity) { //creation of CD requires: initial deposit, interest applied, and end date of CD
         //maturity should be in 'YEAR' form. So, five years for full maturity for example: (param.., 5);
         super(customerID, accountNum, balance, accountFlag);
         interestRate = interest;
         interestEarned = 0;
-        endDate = (maturity * 365);
+        Calendar ed = Calendar.getInstance(); //create a calendar object to set today's date for account creation
+        ed.add(ed.YEAR, (int) maturity);    //sets maturity date
+        longEndDate = ed.getTime().getTime();
+        endDate.setTime(longEndDate);
     }
 
     public CdAccount(int customerID, int accountNum, double balance, int accountFlag, long maturity) { //creation of CD requires: initial deposit, interest applied, and end date of CD
         super(customerID, accountNum, balance, accountFlag);
         interestEarned = 0;
-        endDate = (maturity * 365);;
+        Calendar ed = Calendar.getInstance();
+        ed.add(ed.YEAR, (int) maturity);
+        longEndDate = ed.getTime().getTime();
+        endDate.setTime(longEndDate);
     }
 
-    public long getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(long endDate) {
-        this.endDate = endDate;
+    public CdAccount(int customerID, int accountNum, double balance, int accountFlag, double interest) { //creation of CD requires: initial deposit, interest applied, and end date of CD
+        super(customerID, accountNum, balance, accountFlag);
+        interestEarned = 0;
+        interestRate = interest;
+        Calendar ed = Calendar.getInstance();
+        ed.add(ed.YEAR, (int) FIXEDMAT);
+        longEndDate = ed.getTime().getTime();
+        endDate.setTime(longEndDate);
     }
 
     public CdAccount(int customerID, int accountNum, double balance, int accountFlag) { //creation of CD requires: initial deposit, interest applied, and end date of CD
         super(customerID, accountNum, balance, accountFlag);
         interestEarned = 0;
-        endDate = new Date().getTime() + FIXEDMAT;
+        Calendar ed = Calendar.getInstance();
+        ed.add(ed.YEAR, (int) FIXEDMAT);
+        longEndDate = ed.getTime().getTime();
+        endDate.setTime(longEndDate);
+    }
+
+    //-----------------------------
+    //      Methods
+    //-----------------------------
+    public long getLongRolloverDate() {
+        return longRolloverDate;
+    }
+
+    public void setLongRolloverDate(long longRolloverDate) {
+        this.longRolloverDate = longRolloverDate;
+    }
+
+    public Date getRolloverDate() {
+        Calendar cr = Calendar.getInstance();
+        cr.setTime(this.rolloverDate);
+        return cr.getTime();
+    }
+
+    public void setRolloverDate(long longRolloverDate) {
+        this.rolloverDate.setTime(longRolloverDate);
+    }
+
+    public long getLongEndDate() {
+        return longEndDate;
+    }
+
+    public void setLongEndDate(long endDate) {
+        this.longEndDate = endDate;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double i) {
+        interestRate = i;
+    }
+
+    public double getInterestEarned() {
+        return interestEarned;
+    }
+
+    public Date getEndDate() {
+        Calendar cr = Calendar.getInstance();
+        cr.setTime(this.endDate);
+        return cr.getTime();
+    }
+
+    public void setEndDate(long longEndDate) {
+        this.endDate.setTime(longEndDate);
     }
 
     public double calculateInterest() { //calculates the interest to apply to account, applies it and returns the total applied
@@ -56,39 +129,31 @@ public class CdAccount extends AbstractAccount {
         //a cyclic rate aswell? (3mos, 6mos, etc,)
     }
 
-    public double getInterestRate() {
-        return interestRate;
-    }
-
     public void sendRolloverNotices() {
         //Have to look up notifiable accounts to send notices to
         //and then appriopriately flag said accounts.
         //-Rollover I assume is taking balance of previous account
         //and creating a new CD, putting interestEarned into
         //users other avaliable accounts
-        System.out.println("Account owner's notified");
+        Date currentDate = new Date();
+        if (currentDate.getTime() < endDate.getTime()) {
+            System.out.println("Account has not reached full maturity, no need to send notice");
+        } else {
+            this.accountFlag = 1; // account flagged
+            longRolloverDate = endDate.getTime() + 864000000; // milliseconds at the moment
+            setRolloverDate(longRolloverDate);
+            System.out.println("Account owner has ten days to take action before current CD rolls over into a new CD");
+        }
     }
 
-    public void setInterestRate(double i) {
-        interestRate = i;
-    }
-
-    public double getInterestEarned() {
-        return interestEarned;
-    }
-
-    public double closeCD(double amount, int month, int year, int day) { //closes account, requests current mm/dd/yyyy, return funds depending on maturity
-        long currDate = new Date().getTime(); //
-        if (currDate < endDate) {
+    public double closeCD() { //closes account, requests current mm/dd/yyyy, return funds depending on maturity
+        long currDate = new Date().getTime();
+        if (currDate < endDate.getTime()) {
             balance -= (PENALTY + interestEarned);
+            System.out.println("Penalty for early withdrawal");
             return balance;
         } else {
             return balance;
         }
     }
-
-//    @Override
-//    public void updateBalance(double balance) {
-//        this.balance = balance;
-//    }
 }
