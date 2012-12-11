b085bec37b7e03bea72526581f3f4ff2f839bb29
Martin <mlau@missouriwestern.edu>
2012-12-11 03:04:16 -0600

    accounts with javadocs

--- DatabaseParser/src/bankingsystem/CdAccount.java
+++ DatabaseParser/src/bankingsystem/CdAccount.java
@@ -33,8 +33,21 @@ public class CdAccount extends AbstractAccount {
     //---------------------------------
     //  Constructor
     //---------------------------------
+    /**
+     * most specific constructor for a CD account, requiring the most parameters
+     *
+     * @param customerID is the SSN or ID of a customer
+     * @param accountNum is the account number of the savings account to create
+     * @param balance is the initial balance of the creating account
+     * @param accountFlag is the initial flag of an account; this should ideally
+     * be set to 0 as any account being flagged indicates a problem
+     * @param interest is the interest you personally specify for this account
+     * @param maturity is a int which will indicate that amount of years it will
+     * take for the CD to fully mature (IE: 5 = 5 years from current date; -5 =
+     * 5 years BACK TO THE FUTURE!)
+     */
     public CdAccount(int customerID, int accountNum, double balance, int accountFlag, double interest, int maturity) { //creation of CD requires: initial deposit, interest applied, and end date of CD
-        //maturity should be in 'YEAR' form. So, five years for full maturity for example: (param.., 5);
+
         super(customerID, accountNum, balance, accountFlag);
         interestRate = interest;
         interestEarned = 0;
@@ -47,6 +60,19 @@ public class CdAccount extends AbstractAccount {
         super.setAccountType(accountType);
     }
 
+    /**
+     * less specific constructor for a CD account, not requiring the interest
+     * which will automatically be set to .15
+     *
+     * @param customerID is the SSN or ID of a customer
+     * @param accountNum is the account number of the savings account to create
+     * @param balance is the initial balance of the creating account
+     * @param accountFlag is the initial flag of an account; this should ideally
+     * be set to 0 as any account being flagged indicates a problem
+     * @param maturity is a int which will indicate that amount of years it will
+     * take for the CD to fully mature (IE: 5 = 5 years from current date; -5 =
+     * 5 years BACK TO THE FUTURE!)
+     */
     public CdAccount(int customerID, int accountNum, double balance, int accountFlag, int maturity) { //creation of CD requires: initial deposit, interest applied, and end date of CD
         super(customerID, accountNum, balance, accountFlag);
         interestEarned = 0;
@@ -59,6 +85,17 @@ public class CdAccount extends AbstractAccount {
         super.setAccountType(accountType);
     }
 
+    /**
+     * less specific constructor for a CD account, not requiring the maturity
+     * which will automatically be set to 5 years
+     *
+     * @param customerID is the SSN or ID of a customer
+     * @param accountNum is the account number of the savings account to create
+     * @param balance is the initial balance of the creating account
+     * @param accountFlag is the initial flag of an account; this should ideally
+     * be set to 0 as any account being flagged indicates a problem
+     * @param interest is the interest you personally specify for this account
+     */
     public CdAccount(int customerID, int accountNum, double balance, int accountFlag, double interest) { //creation of CD requires: initial deposit, interest applied, and end date of CD
         super(customerID, accountNum, balance, accountFlag);
         interestEarned = 0;
@@ -72,6 +109,17 @@ public class CdAccount extends AbstractAccount {
         super.setAccountType(accountType);
     }
 
+    /**
+     * is the least specific constructor for a CD account, automatically setting
+     * the interest to .15 and not requiring the maturity which will
+     * automatically be set to 5 years
+     *
+     * @param customerID is the SSN or ID of a customer
+     * @param accountNum is the account number of the savings account to create
+     * @param balance is the initial balance of the creating account
+     * @param accountFlag is the initial flag of an account; this should ideally
+     * be set to 0 as any account being flagged indicates a problem
+     */
     public CdAccount(int customerID, int accountNum, double balance, int accountFlag) { //creation of CD requires: initial deposit, interest applied, and end date of CD
         super(customerID, accountNum, balance, accountFlag);
         interestEarned = 0;
@@ -87,70 +135,157 @@ public class CdAccount extends AbstractAccount {
     //-----------------------------
     //      Methods
     //-----------------------------
+    /**
+     * getter method to return the rollover date of a CD in milliseconds
+     *
+     * @return longRolloverDate
+     */
     public long getLongRolloverDate() {
         return longRolloverDate;
     }
 
+    /**
+     * setter method to set the rollover date of a CD in milliseconds
+     *
+     * @param longRolloverDate
+     */
     public void setLongRolloverDate(long longRolloverDate) {
         this.longRolloverDate = longRolloverDate;
     }
 
+    /**
+     * getter method to return the rollover date of a CD in Date() format
+     *
+     * @return Rollover date in Date() format
+     */
     public Date getRolloverDate() {
         Calendar cr = Calendar.getInstance();
         cr.setTime(this.rolloverDate);
         return cr.getTime();
     }
 
+    /**
+     * setter method to set the rollover date of a CD in Date() format, from a
+     * long (milliseconds)
+     *
+     * @param longRolloverDate
+     */
     public void setRolloverDate(long longRolloverDate) {
         this.rolloverDate.setTime(longRolloverDate);
     }
 
+    /**
+     * getter method to return the creation date of a CD in milliseconds
+     *
+     * @return longStartDate
+     */
     public long getLongStartDate() {
         return longStartDate;
     }
 
+    /**
+     * setter method to set the creation date of a CD in long format, from a
+     * long (milliseconds)
+     *
+     * @param longStartDate
+     */
     public void setLongStartDate(long longStartDate) {
         this.longStartDate = longStartDate;
     }
 
+    /**
+     * getter method to return the creation date of a CD in Date() format
+     *
+     * @return Creation date in Date() format
+     */
     public Date getStartDate() {
         return startDate;
     }
 
+    /**
+     * setter method to set the creation date of a CD in Date() format, from a
+     * long (milliseconds)
+     *
+     * @param startDate
+     */
     public void setStartDate(long startDate) {
         this.startDate.setTime(startDate);
     }
 
+    /**
+     * getter method to return the maturity date of a CD in milliseconds
+     *
+     * @return longEndDate
+     */
     public long getLongEndDate() {
         return longEndDate;
     }
 
+    /**
+     * setter method to set the maturity date of a CD in milliseconds
+     *
+     * @param endDate
+     */
     public void setLongEndDate(long endDate) {
         this.longEndDate = endDate;
     }
 
+    /**
+     * getter method to return CD account interest rate
+     *
+     * @return interestRate
+     */
     public double getInterestRate() {
         return interestRate;
     }
 
+    /**
+     * setter method to set CD account interest rate
+     *
+     * @param i is the new rate you intend to set for the account
+     */
     public void setInterestRate(double i) {
         interestRate = i;
     }
 
+    /**
+     * getter method to return amount earned through interest from the CD
+     *
+     * @return interestEarned
+     */
     public double getInterestEarned() {
         return interestEarned;
     }
 
+    /**
+     * getter method to return the maturity date of a CD in Date() format
+     *
+     * @return Maturity date in Date() format
+     */
     public Date getEndDate() {
         Calendar cr = Calendar.getInstance();
         cr.setTime(this.endDate);
         return cr.getTime();
     }
 
+    /**
+     * setter method to set the maturity date of a CD in Date() format, from a
+     * long (milliseconds)
+     *
+     * @param longEndDate
+     */
     public void setEndDate(long longEndDate) {
         this.endDate.setTime(longEndDate);
     }
 
+    /**
+     * A method to calculate the interest earned on a CD account and add it to
+     * the current balance. Will also store the amount of interest earned into
+     * the interestEarned variable. It will return the amount of interest
+     * earned, but not the updated balance.
+     *
+     * @return total
+     */
     public double calculateInterest() { //calculates the interest to apply to account, applies it and returns the total applied
         double total = (balance * interestRate);
         balance += total;
@@ -160,6 +295,13 @@ public class CdAccount extends AbstractAccount {
         //a cyclic rate aswell? (3mos, 6mos, etc,)
     }
 
+    /**
+     * A method to determine if an account requires a notice to be rolled over.
+     * If an account does require to be rollover over, it's flag will be set and
+     * the rollover date will also be set to ten days from the date the notices
+     * were sent out.
+     *
+     */
     public void sendRolloverNotices() {
         //Have to look up notifiable accounts to send notices to
         //and then appriopriately flag said accounts.
@@ -177,6 +319,14 @@ public class CdAccount extends AbstractAccount {
         }
     }
 
+    /**
+     * A method to close CDs based on the maturity date. If the maturity date
+     * has been passed, no penalties will be incurred. Otherwise, a penalty will
+     * be applied and all interest earned will be lost from the CD.
+     *
+     * @return temp which is end balance of the CD after penalty or after
+     * successful CD closure
+     */
     public double closeCD() { //closes account, requests current mm/dd/yyyy, return funds depending on maturity
 
         double temp = balance; //a temp variable to return the balance, since the balance will be set to zero at the end of method
--- DatabaseParser/src/bankingsystem/SavingsAccount.java
+++ DatabaseParser/src/bankingsystem/SavingsAccount.java
@@ -20,6 +20,19 @@ public class SavingsAccount extends AtmCards {
     //--------------------------------------
     //  Constructors
     //--------------------------------------
+    /**
+     * most specific constructor for a savings account, requiring the most
+     * parameters
+     *
+     * @param customerID is the SSN or ID of a customer
+     * @param accountNum is the account number of the savings account to create
+     * @param balance is the initial balance of the creating account
+     * @param accountFlag is the initial flag of an account; this should ideally
+     * be set to 0 as any account being flagged indicates a problem
+     * @param interest is the interest you personally specify for this account
+     * @param OP is a boolean as to whether or not this savings account will
+     * become an overdraft protection account
+     */
     public SavingsAccount(int customerID, int accountNum, double balance, int accountFlag, double interest, boolean OP) {
         super(customerID, accountNum, balance, accountFlag);
         interestRate = interest;
@@ -27,12 +40,35 @@ public class SavingsAccount extends AtmCards {
         super.setAccountType(accountType);
     }
 
+    /**
+     * less specific constructor for a savings account, automatically setting
+     * the interest to .15
+     *
+     * @param customerID is the SSN or ID of a customer
+     * @param accountNum is the account number of the savings account to create
+     * @param balance is the initial balance of the creating account
+     * @param accountFlag is the initial flag of an account; this should ideally
+     * be set to 0 as any account being flagged indicates a problem
+     * @param OP is a boolean as to whether or not this savings account will
+     * become an overdraft protection account
+     */
     public SavingsAccount(int customerID, int accountNum, double balance, int accountFlag, boolean OP) {
         super(customerID, accountNum, balance, accountFlag);
         isOverdraftAcc = OP;
         super.setAccountType(accountType);
     }
 
+    /**
+     * is the least specific constructor for a savings account, automatically
+     * setting the interest to .15 and indicating that it is not a overdraft
+     * protecting account
+     *
+     * @param customerID is the SSN or ID of a customer
+     * @param accountNum is the account number of the savings account to create
+     * @param balance is the initial balance of the creating account
+     * @param accountFlag is the initial flag of an account; this should ideally
+     * be set to 0 as any account being flagged indicates a problem
+     */
     public SavingsAccount(int customerID, int accountNum, double balance, int accountFlag) {
         super(customerID, accountNum, balance, accountFlag);
         super.setAccountType(accountType);
@@ -41,59 +77,105 @@ public class SavingsAccount extends AtmCards {
     //--------------------------------------
     //  methods
     //--------------------------------------
+    /**
+     * withdraw method which will deduct the amount from balance if amount is less than balance
+     *
+     * @param amount is a value to deduct from the balance
+     * @return updated balance
+     */
     public double withdraw(double amount) { //deducts funds from account, returns updated balance
-        if(amount<=balance){//if the payment is less then or equal to the balence
+        if (amount <= balance) {//if the payment is less then or equal to the balence
             this.DebitAccount(amount);
         }
         return balance;
     }
 
-    public double deposit(double amount) { //adds funds to account, returns updated balance
+    /**
+     * deposit method which will deduct the amount from balance
+     *
+     * @param amount is a value to add to the balance
+     * @return updated balance
+     */
+    public double deposit(double amount) {
         balance += amount;
         return balance;
     }
 
-    public double calculateInterest() { //calculates the interest to apply to account, applies it and returns the total applied
+    /**
+     * calculates the interest based on equation: new total = balance + (balance
+     * * interest)
+     *
+     * @return updated balance
+     */
+    public double calculateInterest() {
         double total = (balance * interestRate);
         balance += total;
         return total;
     }
 
+    /**
+     * getter method to return account protecting from overdraft
+     *
+     * @return accountToProtect
+     */
     public int getAccountToProtect() {
         return accountToProtect;
     }
 
+    /**
+     * setter method to set account to protect from overdraft
+     *
+     * @param accountProtecting is the checking account you intend to protect
+     * from overdrafts
+     */
     public void setAccountToProtect(int accountProtecting) {
         this.accountToProtect = accountProtecting;
     }
 
+    /**
+     * getter method to return savings account interest rate
+     *
+     * @return interestRate
+     */
     public double getInterestRate() {
         return interestRate;
     }
 
+    /**
+     * setter method to set savings account interest rate
+     *
+     * @param rate is the new rate you intend to set for a savings account
+     */
     public void setInterestRate(double rate) {
         interestRate = rate;
     }
 
+    /**
+     * getter method to return if this savings account protects from overdrafts
+     *
+     * @return isOverdraftAcc
+     */
     public boolean isOverdraftAcc() {
         return isOverdraftAcc;
     }
 
+    /**
+     * setter method to set a savings account to protect from overdrafts
+     *
+     * @param set is a boolean, whether or not the account protects another
+     */
     public void setOverdraftAcc(boolean set) {
         isOverdraftAcc = set;
     }
 
-//    @Override
-//    public void updateBalance(double balance) {
-//        this.balance = balance;
-//    }
-
-//    public void print() {
-//        System.out.println(getAccountNum() +" - user num: " + getssn());
-//    }
-//    public double transferFunds(account accNum, double amount) { //you'll have to specify account to pass funds to
-//        balance -= amount;
-//        accNum.balance += amount;        
-//        return balance;
-//    }
+    /**
+     * setter method to quickly update the balance of an account
+     *
+     * @param balance will update accounts balance to the one passed in
+     * parameters
+     */
+    @Override
+    public void updateBalance(double balance) {
+        this.balance = balance;
+    }
 }
